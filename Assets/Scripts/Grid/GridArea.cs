35f88e36b94adfcb93f66c50cebb179cd8b467fa
Jamie Kasulis <jlee.kasulis@gmail.com>
2023-02-23 13:59:57 -0500

    Debug draw the grid

---
+++ Assets/Scripts/Grid/GridArea.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+
+public class GridArea : MonoBehaviour
+{
+    [SerializeField] public int numRows, numCols;
+    public GridCell[,] Cells;
+    [SerializeField] private float CellSize = 1;
+
+    private void Awake()
+    {
+        Debug.Log("Called Awake for GridArea.");
+        transform.position.Set(transform.position.x, transform.position.y, 1f);
+        Cells = new GridCell[numRows, numCols];
+        FillInCells(new Coordinate { row = 0, col = 0 }, new Coordinate { row = numRows, col = numCols });
+    }
+
+    private void Start()
+    {
+        Debug.Log("Called Start for GridArea.");
+    }
+
+    public bool ContainsCoordinate(Coordinate coord)
+    {
+        return coord.row >= 0 && coord.col >= 0 && coord.row < numRows && coord.col < numCols;
+    }
+
+    /**
+     * Fills in the grid from [from, to] with the given content.
+     */
+    private void FillInCells(Coordinate from, Coordinate to)
+    {
+        if (from.row >= to.row || from.col >= to.col) // Can handle this later. But for now, fail. @TODO
+        {
+            throw new System.Exception($"Given invalid args for 'from' and 'to' in FillInCells");
+        }
+
+        (int, int) fillSize = (Mathf.Abs(from.row - to.row), Mathf.Abs(from.col - to.col));
+        if (fillSize.Item1 > numRows || fillSize.Item2 > numCols)
+        {
+            throw new System.Exception($"Given invalid range to fill - it's larger than the grid! Requested range: {from} - {to}");
+        }
+
+        for (int r = from.row; r < to.row; r++)
+        {
+            for (int c = from.col; c < to.col; c++)
+            {
+                GridCell cell = new GridCell(
+                    new Coordinate { row=r, col=c },
+                    transform.position + new Vector3(CellSize * c, CellSize * r, 0)
+                );
+                Cells[r, c] = cell;
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawGridOutline();
+        DrawGridLines();
+    }
+
+    private void DrawGridOutline()
+    {
+        Vector3 topRight = transform.position + new Vector3(CellSize * numCols, 0f, 0f);
+        Vector3 bottomRight = transform.position + new Vector3(CellSize * numCols, -CellSize * numRows, 0f);
+        Vector3 bottomLeft = transform.position + new Vector3(0, -CellSize * numRows, 0f);
+        Gizmos.DrawLine(transform.position, topRight);
+        Gizmos.DrawLine(transform.position, bottomLeft);
+        Gizmos.DrawLine(topRight, bottomRight);
+        Gizmos.DrawLine(bottomLeft, bottomRight);
+        Gizmos.DrawSphere(transform.position, .1f); // Origin
+    }
+
+    private void DrawGridLines()
+    {
+        for (int col = 0; col < numCols; col++)
+        {
+            Gizmos.DrawLine(
+                transform.position + new Vector3(col * CellSize, 0, 0),
+                transform.position + new Vector3(col * CellSize, numRows * -CellSize, 0)
+            );
+        }
+        for (int row = 0; row < numRows; row++)
+        {
+            Gizmos.DrawLine(
+                transform.position + new Vector3(0, row * -CellSize, 0),
+                transform.position + new Vector3(numCols * CellSize, row * -CellSize, 0)
+            );
+        }
+    }
+}
