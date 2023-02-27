08324390fbbd9679b64d5f074f5bc62bb7adb53e
Jamie Kasulis <jlee.kasulis@gmail.com>
2023-02-27 12:18:49 -0500

    Clear all tiles when build mode is disabled.

--- Assets/Scripts/BuildMode/BuildMode.cs
+++ Assets/Scripts/BuildMode/BuildMode.cs
@@ -33,6 +33,10 @@ public class BuildMode : MonoBehaviour
             {
                 PaintBuildableAreaTiles();
             }
+            else
+            {
+                buildModeTilemap.ClearAllTiles();
+            }
         }
 
         if (!isEnabled)
