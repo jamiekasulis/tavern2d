2abf33b8fe464c083ee8cfc4e75b048a0887d3e6
Jamie Kasulis <jlee.kasulis@gmail.com>
2023-03-20 19:30:02 -0400

    Fix build mode styling issue

--- Assets/Scripts/BuildMode/BuildMode.cs
+++ Assets/Scripts/BuildMode/BuildMode.cs
@@ -75,7 +75,7 @@ public class BuildMode : MonoBehaviour
             {
                 OnBuildModeDisabled();
             }
-
+            
             buildModeToggledTrigger.Invoke();
         }
     }
