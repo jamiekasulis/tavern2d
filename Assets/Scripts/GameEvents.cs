40c22ad5c0d6a34255626384e9919fbc993b99c9
Jamie Kasulis <jlee.kasulis@gmail.com>
2023-03-22 15:09:05 -0400

    Create GameEvents file. Reorganize some folders.

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameEvents : MonoBehaviour
+{
+    [SerializeField] private UnityEvent<ItemPickedUpArgs> ItemPickedUp;
+
+    public void OnItemPickedUp(ItemPickedUpArgs args)
+    {
+        ItemPickedUp.Invoke(args);
+    }
+}
+
+public class ItemPickedUpArgs : EventArgs
+{
+    public ItemQuantity iq { get; private set; }
+    public Inventory targetInventory { get; private set; }
+}
