3590e2f04c17d8bd0eaf15383402c84b36f8378a
Jamie Kasulis <jlee.kasulis@gmail.com>
2023-03-24 11:21:14 -0400

    Define some more events

--- Assets/Scripts/Events/GameEvents.cs
+++ Assets/Scripts/Events/GameEvents.cs
@@ -5,11 +5,20 @@ using UnityEngine.Events;
 public class GameEvents : MonoBehaviour
 {
     [SerializeField] private UnityEvent<ItemPickedUpArgs> ItemPickedUp;
+    [SerializeField] private UnityEvent BuildModeEnabled;
+    [SerializeField] private UnityEvent BuildModeDisabled;
 
     public void OnItemPickedUp(ItemPickedUpArgs args)
     {
         ItemPickedUp.Invoke(args);
     }
+
+    public void OnBuildModeEnabled()
+    {
+        BuildModeEnabled.Invoke();
+    }
+
+    
 }
 
 public class ItemPickedUpArgs : EventArgs
